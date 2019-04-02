2adc079635f1e12fc5a5a8e5480a53db99048c68
Colin White <clwhite814@gmail.com>
2019-04-01 19:22:17 -0500

    wip

---
+++ SolidVehicles/Vehicles/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SolidVehicles.Interfaces;
+
+namespace SolidVehicles.Vehicles
+{
+    class Car : IVehicle
+    {
+        public int Wheels { get; set; } = 4;
+        public int Doors { get; set; } = 4;
+        public int PassengerCapacity { get; set; } = 8;
+        public bool Winged { get; set; } = false;
+        public string TransmissionType { get; set; } = "automatic";
+        public double EngineVolume { get; set; } = 100;
+        public double MaxWaterSpeed { get; set; } = 0;
+        public double MaxLandSpeed { get; set; } = 200;
+        public double MaxAirSpeed { get; set; } = 0.0;
+
+        public void Drive()
+        {
+            Console.WriteLine("The jetski zips through the waves with the greatest of ease");
+        }
+
+        public void Fly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
